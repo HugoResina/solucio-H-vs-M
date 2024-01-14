6c14f92bb9c13b187b531fdf8386dd8a68f07f8d
hresi <hresi@HUGORESINA>
2024-01-14 22:58:41 +0100

    Agregar archivos de proyecto.

---
+++ Herois vs Monstre 2 Hugo Resina/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using metodesHVM;
+namespace heroisContraMonstres
+{
+    public class programa
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(C1HVM.recsuma(5)); 
+        }
+    }
+}
---
+++ metodesHVM/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodesHVM
+{
+    public class C1HVM
+    {
+        public static int suma (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int recsuma(int num)
+        {
+            if (num == 1 || num == 0)
+            {
+                return 1;
+            }
+            return num * recsuma(num - 1);
+        }
+    }
+}
