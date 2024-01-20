244131531f7668826550f1b28eb19cc17c929ee7
hresi <hugo.resina.7e7@itb.cat>
2024-01-20 18:35:39 +0100

    intent implementacio estadistiques#7

--- Herois vs Monstre 2 Hugo Resina/Program.cs
+++ Herois vs Monstre 2 Hugo Resina/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Data;
 using metodesHVM;
 namespace heroisContraMonstres
 {
-    public class programa
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(C1HVM.recsuma(5)); 
+            //programa main
+
+            //misatges
+            const string startFight = "1. Iniciar una nova batalla";
+            const string getOut = "0. Sortir";
+            const string insertStat = "introdueix {0} de {1},recorda, el valor ha de estar entre {2} i {3}:";
+
+            //stats
+            string stat = " la força ";
+            string role = " l'arquer ";
+            int validatorFirstMenu, min, max, statValue;
+            min = 1; 
+            max = 10;
+            statValue = 0;
+
+
+            Console.WriteLine(startFight + "\n" + getOut);
+            validatorFirstMenu = Convert.ToInt16(Console.ReadLine());
+            if (validatorFirstMenu == 1)
+            {
+                //part del programa per ficar creacio torns etc
+                do
+                {
+
+                    Console.WriteLine(insertStat, stat, role, min, max);
+                    statValue = Convert.ToInt32(Console.ReadLine());
+                    
+
+                } while (C1HVM.createStat(5, 10, statValue));
+            }
+            else
+            {
+                //directament no entra i et dona gracies
+                Console.WriteLine("gracies per jugar");
+            }
+
         }
     }
 }
--- metodesHVM/Class1.cs
+++ metodesHVM/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,17 +9,11 @@ namespace metodesHVM
 {
     public class C1HVM
     {
-        public static int suma (int num1, int num2)
-        {
-            return num1 + num2;
-        }
-        public static int recsuma(int num)
-        {
-            if (num == 1 || num == 0)
-            {
-                return 1;
-            }
-            return num * recsuma(num - 1);
+       public static bool createStat(int min, int max, int value)
+       {
+
+            return (value >= min && value <= max) ? true : false;
+
         }
     }
 }
