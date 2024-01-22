aca8896f55ab80e487526f5d98e807dce5c26da0
hresi <hugo.resina.7e7@itb.cat>
2024-01-22 20:19:52 +0100

    creador de turns#11

--- metodesHVM/programtest.cs
+++ metodesHVM/programtest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,57 @@ namespace metodesHVM
 {
     public class C1HVM
     {
-        public static bool createStat(int min, int max, int value)
+        public static bool CreateStat(int min, int max, int value)
         {
             return (value >= min && value <= max) ? true : false;
         }
-        public static int randomStat(int min, int max) 
+        public static int RandomStat(int min, int max) 
         {
             Random random = new Random();
             return random.Next(min, max + 1);
         }
-        
+        public static int[] turnCreator() 
+        {
+            int[] turnsArray = { 1, 2, 3, 4 };
+            Random random = new Random();
+
+            
+            for (int i = 0; i < turnsArray.Length - 1; i++)
+            {
+                int j = random.Next(i, turnsArray.Length);
+                int temp = turnsArray[i];
+                turnsArray[i] = turnsArray[j];
+                turnsArray[j] = temp;
+            }
+
+            return turnsArray;
+        }
+        public static string turnSelector(int numero)
+        {
+            string character = "";
+
+            switch (numero)
+            {
+                case 1:
+                    //ataca la arquera
+                    character = "A";
+                    break;
+                case 2:
+                    character = "B";
+                    break;
+                case 3:
+                    character = "M";
+                    break;
+                case 4:
+                    character = "D";
+                    break;
+            }
+
+            return character;
+        }
+        /*public static int Attack()
+        {
+
+        }*/
     }
 }
